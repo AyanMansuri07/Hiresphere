70706fc56a664b94bdcde86da8552274f43a242b
Ayan Mansuri (Ghost_.) <mansuriayan243@gmail.com>
2025-12-30 14:48:56 +0530

    new file:   Profile 	modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Run database migrations
 python manage.py migrate
-
-# Start Gunicorn server
-gunicorn Hiresphere.wsgi:application
+python manage.py collectstatic --noinput
+gunicorn Hireshpere.wsgi:application
